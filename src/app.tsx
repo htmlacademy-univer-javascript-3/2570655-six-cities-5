e06184cce2be9add1599845d75116b656b70d3cf
hovrychev <a.hovrychev@mail.ru>
2024-10-10 19:59:40 +0500

    fix lint

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,11 @@
-import MainScreen from "./pages/Main"
+import MainScreen from './pages/Main';
 
 type AppScreenProps = {
   placesCount: number;
 }
-  
-  function App(props: AppScreenProps): JSX.Element {
-    return (
-      <MainScreen placesCount={props.placesCount} />
-    );
-  }
-  
-  export default App;
+
+function App(props: AppScreenProps): JSX.Element {
+  return <MainScreen placesCount={props.placesCount} />;
+}
+
+export default App;
