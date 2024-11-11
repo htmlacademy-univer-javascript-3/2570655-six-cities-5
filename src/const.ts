905ee47f3a8ba77aaa45a94b3629a6f6cd44f166
hovrychev <a.hovrychev@mail.ru>
2024-11-11 22:51:19 +0500

    complete task-5.1

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,5 @@
+import {Icon} from 'leaflet';
+
 export enum AppRoute {
   Login = '/login',
   Main = '/',
@@ -11,3 +13,28 @@ export enum AuthorizationStatus {
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN',
 }
+
+const URL_MARKER_DEFAULT =
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+
+const URL_MARKER_CURRENT =
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+
+export const defaultCustomIcon = new Icon({
+  iconUrl: URL_MARKER_DEFAULT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
+});
+
+export const currentCustomIcon = new Icon({
+  iconUrl: URL_MARKER_CURRENT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
+});
+
+export const CITY = {
+  title: 'Амстердам',
+  lat: 52.370228,
+  lng: 4.902137,
+  zoom: 11,
+};
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,8 @@
-import {ReactElement} from 'react';
+import {ReactElement, useState} from 'react';
 import {OffersList} from '../components/offersList.tsx';
 import {Offers} from '../types/offer.ts';
 import {HeaderNav} from '../components/heander-nav.tsx';
+import Map from '../components/map.tsx';
 
 type MainScreenProps = {
     placesCount: number;
@@ -9,6 +10,10 @@ type MainScreenProps = {
 }
 
 function MainScreen({placesCount, offers}: MainScreenProps) : ReactElement {
+  const [activeOfferId, setActiveOfferId] = useState<string | null>(null);
+
+  const selectedOffer = offers.find((offer) => offer.id === activeOfferId);
+
   return(
     <div className="page page--gray page--main">
       <header className="header">
@@ -29,6 +34,7 @@ function MainScreen({placesCount, offers}: MainScreenProps) : ReactElement {
           </div>
         </div>
       </header>
+
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
@@ -71,39 +77,30 @@ function MainScreen({placesCount, offers}: MainScreenProps) : ReactElement {
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">
-                {placesCount} places to stay in Amsterdam
-              </b>
+              <b className="places__found">{placesCount} places to stay in Amsterdam</b>
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
                 <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
+                        Popular
+                  <svg className="places__sorting-arrow" width="7" height="4">
+                    <use xlinkHref="#icon-arrow-select"></use>
                   </svg>
                 </span>
                 <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
+                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+                  <li className="places__option" tabIndex={0}>Price: low to high</li>
+                  <li className="places__option" tabIndex={0}>Price: high to low</li>
+                  <li className="places__option" tabIndex={0}>Top rated first</li>
                 </ul>
               </form>
-              <OffersList offers={offers}/>
+              <OffersList offers={offers} changeActiveOffer={setActiveOfferId}/>
             </section>
             <div className="cities__right-section">
-              <section className="cities__map map" />
+              <Map
+                city={offers[0].city}
+                offers={offers}
+                selectedOffer={selectedOffer}
+              />
             </div>
           </div>
         </div>
